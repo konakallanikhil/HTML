46aa991205c8ad5ebcea32f862724c6eb3263f5a
konakallanikhil <129500340+konakallanikhil@users.noreply.github.com>
2024-02-22 11:44:57 +0530

    Completed the project of creating the calculator application project using HTML CSS and Javascript

---
+++ javascript/Projects/calculator.js
@@ -0,0 +1,74 @@
+let currentdisplay = '';
+document.getElementById('display').value = currentdisplay;
+
+
+function one(){
+    currentdisplay = currentdisplay + '1';
+    document.getElementById('display').value = currentdisplay;
+}
+function two(){
+    currentdisplay = currentdisplay + '2';
+    document.getElementById('display').value = currentdisplay;
+}
+function three(){
+    currentdisplay = currentdisplay + '3';
+    document.getElementById('display').value = currentdisplay;
+}
+function four(){
+    currentdisplay = currentdisplay + '4';
+    document.getElementById('display').value = currentdisplay;
+}
+function five(){
+    currentdisplay = currentdisplay + '5';
+    document.getElementById('display').value = currentdisplay;
+}
+function six(){
+    currentdisplay = currentdisplay + '6';
+    document.getElementById('display').value = currentdisplay;
+}
+function seven(){
+    currentdisplay = currentdisplay + '7';
+    document.getElementById('display').value = currentdisplay;
+}
+function eight(){
+    currentdisplay = currentdisplay + '8';
+    document.getElementById('display').value = currentdisplay;
+}
+function nine(){
+    currentdisplay = currentdisplay + '9';
+    document.getElementById('display').value = currentdisplay;
+}
+function zero(){
+    currentdisplay = currentdisplay + '0';
+    document.getElementById('display').value = currentdisplay;
+}
+function add(){
+    currentdisplay = currentdisplay + '+';
+    document.getElementById('display').value = currentdisplay;
+}
+function minus(){
+    currentdisplay = currentdisplay + '-';
+    document.getElementById('display').value = currentdisplay;
+}
+function star(){
+    currentdisplay = currentdisplay + '*';
+    document.getElementById('display').value = currentdisplay;
+}
+function div(){
+    currentdisplay = currentdisplay + '/';
+    document.getElementById('display').value = currentdisplay;
+}
+function point(){
+    currentdisplay = currentdisplay + '.';
+    document.getElementById('display').value = currentdisplay;
+}
+function equal(){
+    currentdisplay = eval(currentdisplay);
+    document.getElementById('display').value = currentdisplay;
+}
+function bin() {
+    currentdisplay = '';
+    document.getElementById('display').value = currentdisplay;
+}
+
+
